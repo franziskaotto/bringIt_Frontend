b1b5e7328596d76fc32544f282c10c99fd2075d8
franziskaotto <franziska.otto@gmx.at>
2024-06-03 18:56:25 +0200

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <p>
+       initial commit
+      </p>
+    </>
+  )
+}
+
+export default App
