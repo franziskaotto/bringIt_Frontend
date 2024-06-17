7426d610155696e1eef57d478823f9fa692e28e8
franziskaotto <franziska.otto@gmx.at>
2024-06-17 20:54:21 +0200

    get device location and implemented in map

--- src/Components/TomTomMap/TomTomMap.jsx
+++ src/Components/TomTomMap/TomTomMap.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import tt from "@tomtom-international/web-sdk-maps";
 
@@ -6,18 +6,46 @@ import tt from "@tomtom-international/web-sdk-maps";
 const KEY = "BqBA01CipujA3uzkCYoxjc0RTYKjAMBq";
 
 
+
+const findMyState = (setLatitude, setLongitude) => {
+
+  const success = (position) => {
+
+    setLongitude(position.coords.latitude);
+    setLatitude(position.coords.longitude);
+   
+  }
+
+  const error = () => {
+    console.log("error getting position")
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+}
+
+
 const TomTomMap = () => {
+  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  findMyState(setLatitude, setLongitude);
+
   const mapElement = useRef(null);
-
+  
+  
   useEffect(() => {
-    const map = tt.map({
-      key: KEY,
-      container: mapElement.current,
-      center: [16.372, 48.208], // Coordinates of the map center
-      zoom: 10
-    });
+   
+    if(longitude !== null && latitude !== null) { //hier weiß ich grad nicht ob wir das wirklich brauchen
+      const map = tt.map({
+        key: KEY,
+        container: mapElement.current,
+        center: [latitude, longitude], // Coordinates of the map center
+        zoom: 15,
+      });
+  
+      return () => map.remove();
 
-    return () => map.remove();
+    }
   }, []);
 
   return (
